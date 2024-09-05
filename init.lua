b87831c5134595ce28ebe74f2f9f78007c02b2e4
Joshua Ong <joshuaong2000@gmail.com>
2024-09-05 21:16:18 +1000

    Ergo

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 vim.cmd([[
     set ts=4 sw=4 expandtab smarttab autoindent
     set rnu nu foldmethod=indent hls
-    set autochdir path=** wildmenu wildignore=**/.git/**,**/node_modules/**,**/target/**,**/build/** wildoptions=pum
+    set autochdir path=** wildmenu wildignore=**/.git/**,**/node_modules/**,**/target/**,**/build/**
     let g:netrw_keepdir = 0
     set shortmess-=S hidden nobackup nowritebackup
-    set statusline=%{winnr()}%{'\ '.expand('%:p')}
+    set statusline=%{expand('%:p')}%{'\ \ '.winnr()}
     let mapleader = " "
     nmap <space> <nop>
     nmap <leader>l gt
@@ -20,37 +20,35 @@ vim.cmd([[
     command! -nargs=1 Tm tabmove <args>
     cmap <c-l> <c-r>0
 
-    function! Bd()
+    function! GetBufferDirectory()
         let @0 = expand('%:p')
     endfunction
-    command! Bd call Bd()
+    command! Bd call GetBufferDirectory()
 
-    function! Fw()
+    function! HighlightWord()
         let l:cword = expand('<cword>')
         call clearmatches()
         execute 'match Search /\<\V' . l:cword . '\>/'
     endfunction
-    nnoremap <leader>f :call Fw()<cr>
+    nnoremap <leader>f :call HighlightWord()<cr>
 
-    function! SyncWd()
+    function! SyncWorkingDirectory()
         let l:parent_path = expand('%:p:h')
         if isdirectory(l:parent_path)
             execute 'cd ' . fnameescape(l:parent_path)
         endif
     endfunction
-    nmap <leader>s :call SyncWd()<cr>
+    nmap <leader>s :call SyncWorkingDirectory()<cr>
 
-    function! ExchangeCurrentBufferWithQuickfix()
-        copen
-
+    function! ExchangeWindowBuffers()
         let current_window = winnr()
         let current_buffer = bufnr("%")
         let previous_window = winnr("#")
         let previous_buffer = winbufnr(previous_window)
   
-        exec  previous_window . " wincmd w" . "|" .
-            \ "buffer " . current_buffer . "|" .
-            \ current_window ." wincmd w" . "|" .
+        exec  previous_window . " wincmd w" . " | " .
+            \ "buffer " . current_buffer . " | " .
+            \ current_window ." wincmd w" . " | " .
             \ "buffer " . previous_buffer
 
         q
@@ -58,79 +56,129 @@ vim.cmd([[
         wincmd p  
     endfunction
 
-    let g:sandpit_filename = ".sandpit"
-    let g:find_ignores = join(['-not -path **/.git/**',
+    function! ExchangeCurrentBufferWithQuickfix()
+        copen
+        call ExchangeWindowBuffers()
+    endfunction
+
+    let g:SANDPIT_FILENAME = ".sandpit"
+    let g:FIND_IGNORES = join(['-not -path **/.git/**',
         \ '-not -path **/node_modules/**',
         \ '-not -path **/target/**',
         \ '-not -path **/build/**',
         \ '-not -path **/plugin/**'], ' ')
 
-    function! Fr(...)
+    function! GetStartPosition(ignore_count, vertical_range)
+        let l:ignore_count_ = a:ignore_count
+        let l:start_point = './'
+        for _ in range(1, a:vertical_range)
+            let l:current_ls = systemlist('ls -1 -a ' . start_point)
+            if index(current_ls, g:SANDPIT_FILENAME) != -1 && ignore_count_ == 0
+                break 
+            endif
+            let l:ignore_count_ -= 1
+            let l:start_point = start_point . '../'
+        endfor
+        return start_point
+    endfunction
+
+    function! FindWithinVerticalRange(...)
         let l:ignore_count = 0
-        let l:range_ = 3
-        if a:0 >= 1
-            let l:pattern = a:1
-        else 
+        let l:vertical_range = 3
+        if a:0 >= 1 | let l:pattern = a:1 | else 
             echo "Did not supply a pattern to search for."
             return
         endif
-        if a:0 >= 2
-            let l:ignore_count = a:2
-        endif
-        if a:0 >= 3
-            let l:range_ = a:3
+        if a:0 >= 2 | let l:ignore_count = a:2 | endif
+        if a:0 >= 3 | let vertical_range = a:3 | endif
+        if a:0 >= 4 
+            echo "Too many arguments."
+            return
         endif
 
-        let start_point_ = './'
-        for _ in range(1, l:range_)
-            let current_ls = systemlist('ls -1 -a ' . start_point_)
-            if index(current_ls, g:sandpit_filename) != -1 && l:ignore_count == 0
-                break 
-            endif
-            let l:ignore_count -= 1
-            let start_point_ = start_point_ . '../'
-        endfor
+        let l:start_point = GetStartPosition(ignore_count, vertical_range)
 
-        let start_point = join(repeat([".."], l:range_), '/')
-        let matches = systemlist('find ' . start_point_ .
-            \ ' -maxdepth ' . (l:range_ * 2) .
+        let l:find_matches = systemlist('find ' . start_point .
+            \ ' -maxdepth ' . (vertical_range * 2) .
             \ ' -name ' . shellescape(l:pattern) .
-            \ ' ' . g:find_ignores .
+            \ ' ' . g:FIND_IGNORES .
             \ ' -print')
 
-        if empty(matches)
+        if empty(find_matches)
             echo "No matches found."
             return
         endif
 
-        let matches_ = []
-        for match in matches
-            call add(matches_, { 'filename': match, 'lnum': 1, 'col': 1 })
+        let l:matches_ = []
+        for l:match in find_matches
+            call add(matches_, { 'filename': match })
         endfor
         call setqflist(matches_, 'r')
         
         call ExchangeCurrentBufferWithQuickfix()
     endfunction
-    command! -nargs=* Fr call Fr(<f-args>)
+    command! -nargs=* F call FindWithinVerticalRange(<f-args>)
 
-    function! F(pattern)
-        let matches = systemlist('find .' . 
+    function! Find(pattern)
+        let find_matches = systemlist('find .' . 
             \ ' -name ' . shellescape(a:pattern) .
-            \ ' ' . g:find_ignores .
+            \ ' ' . g:FIND_IGNORES .
             \ ' -print')
-        call append(line('.') - 1, matches)
+        call append(line('.') - 1, find_matches)
     endfunction
-    command! -nargs=* F call F(<f-args>)
+    command! -nargs=* Find call Find(<f-args>)
 
-    function! G(pattern)
-        let matches = systemlist('grep' .
-            \ ' --exclude-dir=.git --exclude-dir=node_modules' .
-            \ ' --exclude-dir=target --exclude-dir=build' .
-            \ ' --exclude-dir=plugin --exclude=' . g:sandpit_filename .
-            \ '-I -r ' . a:pattern)
-        call append(line('.') - 1, matches)
+    let g:GREP_EXCLUDES = join(['--exclude-dir=.git',
+        \ '--exclude-dir=node_modules',
+        \ '--exclude-dir=target --exclude-dir=build',
+        \ '--exclude-dir=plugin --exclude=' . g:SANDPIT_FILENAME], ' ')
+
+    function! GrepWithinVerticalRange(...)
+        let l:ignore_count = 0
+        let l:vertical_range = 3
+        if a:0 >= 1 | let l:pattern = a:1 | else 
+            echo "Did not supply a pattern to search for."
+            return
+        endif
+        if a:0 >= 2 | let l:ignore_count = a:2 | endif
+        if a:0 >= 3 | let l:vertical_range = a:3 | endif
+        if a:0 >= 4 
+            echo "Too many arguments."
+            return
+        endif
+
+        let l:start_point = GetStartPosition(ignore_count, vertical_range)
+
+        let l:grep_matches = systemlist('grep ' . shellescape(l:pattern) .
+            \ ' ' . g:GREP_EXCLUDES .
+            \ ' -I -n -r ' . start_point)
+
+        if empty(grep_matches)
+            echo "No matches found."
+            return
+        endif
+
+        let l:matches_ = []
+        for l:match in grep_matches
+            let l:filepath = matchstr(match, '^[^:]*')
+            let l:line_number = matchstr(match, ':\d*:')
+            let l:line_number = line_number[1 : len(line_number) - 2]
+            let l:code_glimps = substitute(match, '^[^:]*:\d\+:\s*', '', '')
+            call add(matches_, { 'filename': filepath, 'lnum': line_number, 'text': code_glimps })
+        endfor
+        call setqflist(matches_, 'r')
+        
+        call ExchangeCurrentBufferWithQuickfix()
     endfunction
-    command! -nargs=* G call G(<f-args>)
+    command! -nargs=* G call GrepWithinVerticalRange(<f-args>)
+
+    function! Grep(pattern)
+        let l:grep_matches = systemlist('grep' . shellescape(a:pattern) .
+            \ ' ' . g:GREP_EXCLUDES .
+            \ ' -I -n -r ' . start_point)
+        call append(line('.') - 1, grep_matches)
+    endfunction
+    command! -nargs=* Grep call Grep(<f-args>)
 ]])
 
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
